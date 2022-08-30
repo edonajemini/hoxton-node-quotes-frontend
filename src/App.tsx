24daefaf714b83e3d0e2250170238f9c475e9472
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-08-30 15:02:37 +0200

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
+type Quotes = {
+  id: number,
+  author:string,
+  quote: string
+}
 
+function App() {
+  const [quotes, setQuotes] = useState<Quotes[]>([])
+  useEffect(() => {
+    fetch("http://localhost:4000/quotes")
+      .then((resp) => resp.json())
+      .then((quotesFromServer) => setQuotes(quotesFromServer));
+  }, []);
   return (
     <div className="App">
+      {
+            quotes.map(quote => (
+              <div className='quotes'>
+                  <p>{quote.author}-</p>
+                    <h3>{quote.quote}</h3>
+              </div>
+            ))}
     </div>
   )
 }
